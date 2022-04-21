2cfaff9db880ea70795639a19840ecf4a2ef8ccd
Stephen <s.lin@alumni.ubc.ca>
2022-04-20 22:25:59 -0700

    sort function working

---
+++ ballbuddy.py
@@ -0,0 +1,32 @@
+#Functions for suggesting choices
+
+## General
+def load_picks():
+    #loads in everyone's picks
+    return
+
+def save_picks():
+    #save everyone's picks to csv\
+    return
+
+## Team Phase
+def team_analyze():
+    #analyze and evaluate which of the categories has many competetitors and which has less
+    #return a projected ordered list of best to worst picks
+    return
+
+def team_suggest_purchase():
+    #suggest if a purchase is worth it based on everyone's money
+    #suggest if worth it to make someone over pay
+    #return a list of rich to poor
+    return
+
+## Play Phase
+def play_analyze():
+    #analyze the opponent's team and best categories, suggest weak categories and the guys to put up to get there
+    #return a win % guess
+
+def play_swap():
+    #analyze current team, account for injury, and suggest who to sub in
+    #return new cateogory analysis of team strengths for each cateogry
+    return
---
+++ main.py
@@ -0,0 +1,109 @@
+import csv
+from collections import OrderedDict
+from operator import itemgetter, attrgetter
+
+file_name = 'data/2021-2022.csv'
+sort_category = ['PLAYER', 'POS', 'TEAM', 'GP', 'MPG', 'FGM', 'FG%', 'FTM', 'FT%', '3PM', '3P%', 'PTS', 'DREB', 'OREB', 'TREB', 'AST', 'STL', 'BLK', 'TO', 'DD', 'A/TO']
+
+def start():
+    RAWdata = readdata(file_name) #load data
+    sortby(sort_category[-5],RAWdata) #testing funct
+
+def readdata(filename):
+    with open(filename, newline='') as f:
+        datadict=OrderedDict()
+        datadict = [{k: v for k, v in row.items()} #write to a dict
+        for row in csv.DictReader(f, skipinitialspace=True)] #1 liner from above line
+        return datadict
+
+def sortby(sort_category,data):
+    match sort_category:
+        case 'PLAYER':
+            print('Sort: PLAYER')
+            data.sort(key=lambda x:x['PLAYER']) #sort all keys' values, key is 'player' here
+            return data
+        case 'POS':
+            print('Sort: POS')
+            data.sort(key=lambda x:x['POS'])
+            return data
+        case 'TEAM':
+            print('Sort: TEAM')
+            data.sort(key=lambda x:x['TEAM'])
+            return data
+        case 'GP':
+            print('Sort: GP')
+            data.sort(key=lambda x:x['GP'])
+            return data
+        case 'MPG':
+            print('Sort: MPG')
+            data.sort(key=lambda x:x['MPG'])
+            return data
+        case 'FGM':
+            print('Sort: FGM')
+            data.sort(key=lambda x:x['FGM'])
+            return data
+        case 'FG%':
+            print('Sort: FG%')
+            data.sort(key=lambda x:x['FG%'])
+            return data
+        case 'FTM':
+            print('Sort: FTM')
+            data.sort(key=lambda x:x['FTM'])
+            return data
+        case 'FT%':
+            print('Sort: FT%')
+            data.sort(key=lambda x:x['FT%'])
+            return data
+        case '3PM':
+            print('Sort: 3PM')
+            data.sort(key=lambda x:x['3PM'])
+            return data
+        case '3P%':
+            print('Sort: 3P%')
+            data.sort(key=lambda x:x['3P%'])
+            return data
+        case 'PTS':
+            print('Sort: PTS')
+            data.sort(key=lambda x:x['PTS'])
+            return data
+        case 'DREB':
+            print('Sort: DREB')
+            data.sort(key=lambda x:x['DREB'])
+            return data
+        case 'OREB':
+            print('Sort: OREB')
+            data.sort(key=lambda x:x['OREB'])
+            return data
+        case 'TREB':
+            print('Sort: TREB')
+            data.sort(key=lambda x:x['TREB'])
+            return data
+        case 'AST':
+            print('Sort: AST')
+            data.sort(key=lambda x:x['AST'])
+            return data
+        case 'STL':
+            print('Sort: STL')
+            data.sort(key=lambda x:x['STL'])
+            return data
+        case 'BLK':
+            print('Sort: BLK')
+            data.sort(key=lambda x:x['BLK'])
+            return data
+        case 'TO':
+            print('Sort: TO')
+            data.sort(key=lambda x:x['TO'])
+            return data
+        case 'DD':
+            print('Sort: DD')
+            data.sort(key=lambda x:x['DD'])
+            return data
+        case 'A/TO':
+            print('Sort: A/TO')
+            data.sort(key=lambda x:x['A/TO'])
+            return data
+    return
+                
+
+if __name__ =='__main__':
+    start()
